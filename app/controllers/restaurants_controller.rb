34627f1b7edc4102db16792477bf240a3ee9d5f8
Amanda Ng <amandaleahng@gmail.com>
2020-03-11 17:45:57 +0800

    everything but delete function added

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,30 +1,47 @@
 class RestaurantsController < ApplicationController
-  before_action :set_review, only: [:show, :edit, :update, :destroy]
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def create
+    @restaurant = Restaurant.new(restaurant_param)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
   end
 
   def index
+    @restaurants = Restaurant.all
   end
 
   def show
+    @review = Review.new
   end
 
   def edit
   end
 
   def update
+    @restaurant.update(restaurant_param)
+
+    redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
+    @restaurant.destroy
+
+    redirect_to restaurants_path
   end
 
   private
 
   def set_restaurant
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_param
+    params.require(:restaurant).permit(:name, :category, :address, :phone_number)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,20 @@
 class ReviewsController < ApplicationController
+  before_action :set_restaurant, only: [:new, :create]
+
   def new
+    @review = Review.new
   end
 
   def create
+    @review = Review.new(review_param)
+    @review.restaurant = @restaurant
+    @review.save
+
+    redirect_to restaurant_path(@restaurant)
   end
 
   def index
+    @reviews = Review.all
   end
 
   def show
@@ -15,14 +24,24 @@ class ReviewsController < ApplicationController
   end
 
   def update
+    @review.update(review_param)
+
+    redirect_to restaurant_path(@restaurant)
   end
 
   def destroy
+    @review.destroy
+
+    redirect_to reviews_path
   end
 
   private
 
-  def set_review
-    @review = Review.find(params[:review_id])
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+  def review_param
+
   end
 end
